34938669d6a90d3c9c92b320034238dfcf3655ef
Graham Gilbert <graham@grahamgilbert.com>
2014-07-15 21:54:57 +0100

    Ensure the puppet directories are there

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,12 @@
 node default {
+
+    file {'/var/lib/puppet':
+        ensure => directory,
+    }
+
+    file {'/etc/puppet':
+        ensure => directory,
+    }
+
     include mac_admin::login_window_hostname
 }
